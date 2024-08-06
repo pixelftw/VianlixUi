81fda99a2b55346b5fefb9943ae9e46216d5109f
Vikas Yadav <geekyvikas240052@gmail.com>
2024-08-06 23:58:12 +0530

    feat: create magnet component

---
+++ components/motion/magnet.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+import { type MouseEvent as ME, PropsWithChildren, useRef } from "react";
+
+export function Magnet({ children }: PropsWithChildren) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const handleMouseMove = (e: ME<HTMLDivElement, MouseEvent>) => {
+    if (ref.current) {
+      const { clientX, clientY } = e;
+      const { left, top, width, height } = ref.current.getBoundingClientRect();
+
+      x.set(clientX - (left + width / 2));
+      y.set(clientY - (top + height / 2));
+    }
+  };
+
+  const smoothX = useSpring(x, { stiffness: 150, damping: 20, mass: 0.6 });
+  const smoothY = useSpring(y, { stiffness: 150, damping: 20, mass: 0.6 });
+
+  const onMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={onMouseLeave}
+      style={{ x: smoothX, y: smoothY }}
+      className="w-fit"
+    >
+      {children}
+    </motion.div>
+  );
+}
